5c427ee6163878bfc4c36aea53654cd0c80b7acb
Evan <bluhme3@gmail.com>
2020-05-05 22:01:03 -0800

    Deployed 80f6e0f with MkDocs version: 1.1
    Former-commit-id: 267f46f02c1d62b7fb49e5cf187468f04aae9f92 [formerly cfd90e163be09a5eace56a1e76e59e749855deef]
    Former-commit-id: 37f09bed21a6f417fa85102c93e65f15b64171ca

--- extra.js
+++ extra.js
@@ -1,4 +1,4 @@
 // Use this to scroll to the end of the page after the page has finished loading
 
-window.scrollTo(0,document.body.scrollHeight);
+// window.scrollTo(0,document.body.scrollHeight);
 
